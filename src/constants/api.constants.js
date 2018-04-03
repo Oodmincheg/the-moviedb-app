d453e58c65212ffe88676d70a50f4c1d13cbf49b
dmitryRomankov <strmr@tut.by>
2018-04-03 11:26:35 +0300

    constants

---
+++ src/constants/api.constants.js
@@ -0,0 +1,3 @@
+export const apiKey = '578fd6715dcda4650da7acfb7221559d';
+export const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+export const baseUrl = 'https://image.tmdb.org/t/p/w185/';
