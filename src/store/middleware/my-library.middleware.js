c1ade2d81f1e2c7a5111b737d81405f96e611107
dmitryRomankov <strmr@tut.by>
2018-05-22 14:40:24 +0300

    middleware for saving state after adding/deleting from "my-library"

--- src/store/middleware/my-library.middleware.js
+++ src/store/middleware/my-library.middleware.js
@@ -11,22 +11,48 @@ import {
 
 export const myLibraryMiddleware = store => next => action => {
     if (action.type === ADD_MOVIE_TO_LIBRARY) {
-        localStorage.removeItem('movies');
-        let currentMovies = store.getState().moviesReducer.movies;
-        console.log(currentMovies);
-        setItemToLocalStorage('movies', currentMovies);
+        let moviesFromLS = getItemFromLocalStorage('movies') || [];
+        let updatedMovies = moviesFromLS.map(item => {
+            if (item.id === action.payload.id) {
+                item.isInLibrary = true;
+            }
+            return item;
+        });
+        setItemToLocalStorage('movies', updatedMovies);
+    }
+
+    if (action.type === REMOVE_MOVIE_FROM_LIBRARY) {
+        let currentMoviesFromLS = getItemFromLocalStorage('movies') || [];
+        let updatedMoviesAfterDeleting = currentMoviesFromLS.map(item => {
+            if (item.id === action.payload.id) {
+                item.isInLibrary = false;
+            }
+            return item;
+        });
+        setItemToLocalStorage('movies', updatedMoviesAfterDeleting);
     }
 
     if (action.type === ADD_TVSHOW_TO_LIBRARY) {
-        localStorage.removeItem('tvShows');
-        let currentTvShows = store.getState().tvShowsReducer.tvShows;
-        console.log(currentTvShows);
-        setItemToLocalStorage('tvShows', currentTvShows);
+        let tvShowsFromLS = getItemFromLocalStorage('tvShows') || [];
+        let updatedTvShows = tvShowsFromLS.map(item => {
+            if (item.id === action.payload.id) {
+                item.isInLibrary = true;
+            }
+            return item;
+        });
+        setItemToLocalStorage('tvShows', updatedTvShows);
     }
 
-    // if (action.type === REMOVE_TVSHOW_FROM_LIBRARY) {
-
-    // }
+    if (action.type === REMOVE_TVSHOW_FROM_LIBRARY) {
+        let currentTvShowsFromLS = getItemFromLocalStorage('tvShows') || [];
+        let updatedTvShowsAfterDeleting = currentTvShowsFromLS.map(item => {
+            if (item.id === action.payload.id) {
+                item.isInLibrary = false;
+            }
+            return item;
+        });
+        setItemToLocalStorage('tvShows', updatedTvShowsAfterDeleting);
+    }
 
     return next(action);
 }
