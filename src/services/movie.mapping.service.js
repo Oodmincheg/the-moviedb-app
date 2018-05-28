188c8c8083e0f39d62e4b5252fd6bd654de2f456
dmitryRomankov <strmr@tut.by>
2018-05-28 17:26:07 +0300

    changed poster url

--- src/services/movie.mapping.service.js
+++ src/services/movie.mapping.service.js
@@ -5,7 +5,7 @@ export class MovieEntity {
         this.adult = entity.adult || '';
         this.overview = entity.overview || '';
         this.poster = 'https://image.tmdb.org/t/p/w185' + entity.poster_path || '';
-        this.backdrop = 'https://image.tmdb.org/t/p/w500' + entity.backdrop_path || '';
+        this.backdrop = 'https://image.tmdb.org/t/p/w342' + entity.poster_path || '';
         this.adult = entity.adult || '';
         this.genre_ids = entity.genre_ids || '';
         this.vote = entity.vote_average || '';
