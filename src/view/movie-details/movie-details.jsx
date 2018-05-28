9d73f0118aaf60bc4b90353c7a438b178f9c14ce
dmitryRomankov <strmr@tut.by>
2018-05-28 17:30:02 +0300

    added genres and description in this components

--- src/view/movie-details/movie-details.jsx
+++ src/view/movie-details/movie-details.jsx
@@ -2,29 +2,53 @@ import React, { Component } from 'react';
 import './movie-details.css';
 import { connect } from 'react-redux';
 import { fetchMovies } from '../../store/actions';
+import { GenresList } from '../../components/genres';
+import { getItemFromLocalStorage } from '../../services';
 
 export class MovieDetails extends Component {
 	constructor(props) {
 		super(props)
-		this.props.fetchMovies();
+		this.state = {
+			genresFromLS: getItemFromLocalStorage('genres'),
+			movie: {},
+			recommended: []
+		}
 	}
+
+	initMovieAndRecommended(id) {
+		let movies = getItemFromLocalStorage('movies').filter(movie => movie.id === parseInt(id, 10));
+		this.setState(() => ({
+			movie: movies[0]
+		}));
+	}
+
+	componentWillMount() {
+		this.initMovieAndRecommended(this.props.match.params.id);
+	}
+
 	render() {
-		const movie = this.props.movies.filter(m => m.id == this.props.match.params.id);
+		let movie = this.state.movie;
+		let genres = this.state.genresFromLS.filter(genre => movie.genre_ids.includes(genre.id));
 		return (
-			<div className='mdb-movie__details'>
-				{movie.map(m =>
-					<div
-						className='mdb-movie__info'
-						key={m.id}
-					>
-						<img
-							src={m.backdrop}
-							alt={m.title}
-							key={m.id * 2}
-						/>
-						<h1 key={m.id / 2}>{m.title}</h1>
+			<div className='mdb-details__view'>
+				<div className='mdb-details__wrapper'>
+					<img
+						className='mdb-details__backdrop'
+						src={movie.backdrop}
+						alt={movie.title}
+					/>
+					<div className='mdb-details__movie-info'>
+						<h2 className='mdb-details__title'>{movie.title}</h2>
+						<div className='mdb-details__overview'>
+							<p className=''>{movie.overview}</p>
+							<p>{movie.overview}</p>
+							<p>{movie.overview}</p>
+							<p>{movie.overview}</p>
+						</div>
 					</div>
-				)}
+					<p>Genre</p>
+					<GenresList compareGenres={genres.map(genre => genre.name)} />
+				</div>
 			</div>
 		);
 	}
@@ -32,7 +56,8 @@ export class MovieDetails extends Component {
 
 const mapStateToProps = (state) => {
 	return {
-		movies: state.moviesReducer.movies
+		movies: state.moviesReducer.movies,
+		genres: state.genresReducer.genres
 	}
 }
 
--- src/view/tvshow-details/tvshow-details.jsx
+++ src/view/tvshow-details/tvshow-details.jsx
@@ -1,47 +1,72 @@
 import React, { Component } from 'react';
-import './tvshow-details.css';
 import { connect } from 'react-redux';
 import { fetchTvShows } from '../../store/actions';
+import { getItemFromLocalStorage } from '../../services';
+import { GenresList } from '../../components/genres';
+import './tvshowdetail.scss';
 
 export class TvShowDetails extends Component {
-    constructor(props) {
-        super(props)
-        this.props.fetchTvShows();
-    }
-    render() {
-        const tvShow = this.props.tvShows.filter(t => t.id == this.props.match.params.id);
-        return (
-            <div className='mdb-tvshow__details'>
-                {tvShow.map(t =>
-                    <div
-                        className='mdb-tvshow__info'
-                        key={t.id}
-                    >
-                        <img
-                            src={t.backdrop}
-                            alt={t.title}
-                            key={t.id * 2}
-                        />
-                        <h1 key={t.id / 2}>{t.title}</h1>
-                    </div>
-                )}
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			genresFromLS: getItemFromLocalStorage('genres'),
+			tvShow: {},
+			recommended: []
+		}
+
+	}
+
+	initTvShowAndRecommended(id) {
+		let tvShows = getItemFromLocalStorage('tvShows').filter(tvshow => tvshow.id === parseInt(id, 10));
+		this.setState(() => ({
+			tvShow: tvShows[0]
+		}));
+	}
+
+	componentWillMount() {
+		this.initTvShowAndRecommended(this.props.match.params.id);
+	}
+
+	render() {
+		const tvShow = this.state.tvShow;
+		let genres = this.state.genresFromLS.filter(genre => tvShow.genre_ids.includes(genre.id));
+		return (
+			<div className='mdb-details__view'>
+				<div className='mdb-details__wrapper'>
+					<img
+						className='mdb-details__backdrop'
+						src={tvShow.backdrop}
+						alt={tvShow.title}
+					/>
+					<div className='mdb-details__movie-info'>
+						<h2 className='mdb-details__title'>{tvShow.title}</h2>
+						<div className='mdb-details__overview'>
+							<p className=''>{tvShow.overview}</p>
+							<p>{tvShow.overview}</p>
+							<p>{tvShow.overview}</p>
+							<p>{tvShow.overview}</p>
+						</div>
+					</div>
+					<p>Genre</p>
+					<GenresList compareGenres={genres.map(genre => genre.name)} />
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-    return {
-        tvShows: state.tvShowsReducer.tvShows
-    }
+	return {
+		tvShows: state.tvShowsReducer.tvShows
+	}
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchTvShows: () => {
-            dispatch(fetchTvShows());
-        }
-    }
+	return {
+		fetchTvShows: () => {
+			dispatch(fetchTvShows());
+		}
+	}
 }
 
 export const TvShowDetailsPage = connect(mapStateToProps, mapDispatchToProps)(TvShowDetails);
