08d7caf6e63f893ed01616cbc48fdeb1bda5d97c
dmitryRomankov <strmr@tut.by>
2018-06-01 12:05:42 +0300

    added service for similar shows and movies

--- src/services/data.service.js
+++ src/services/data.service.js
@@ -1,3 +1,5 @@
+import { similarMovies, similarTvShows } from '../constants';
+
 export const getData = (url) => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
     request.open('GET', url);
@@ -30,3 +32,36 @@ export const getGenresData = (url) => new Promise((resolve, reject) => {
     request.send();
 });
 
+export const getSimilarMovies = (movie_id) => new Promise((resolve, reject) => {
+    const url = similarMovies(movie_id);
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = () =>
+    request.status === 200 ?
+    resolve(
+        typeof request.response === 'string' ?
+        JSON.parse(request.response).results :
+        request.response.results
+    )
+    :
+    reject(Error(request.statusText));
+    request.onerror = (err) => reject(err);
+    request.send();
+});
+
+export const getSimilarTvShows = (tv_id) => new Promise((resolve, reject) => {
+    const url = similarTvShows(tv_id);
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = () =>
+    request.status === 200 ?
+    resolve(
+        typeof request.response === 'string' ?
+        JSON.parse(request.response).results :
+        request.response.results
+    )
+    :
+    reject(Error(request.statusText));
+    request.onerror = (err) => reject(err);
+    request.send();
+});
