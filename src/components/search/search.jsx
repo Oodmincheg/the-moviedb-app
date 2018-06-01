411600d9707cec0cea648efa06f5be56fe66b9da
dmitryRomankov <strmr@tut.by>
2018-06-01 12:04:12 +0300

    refactor: changed classNames in component

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import './search.css';
+import './search.scss';
 
 export const Search = () => {
 	return (
-		<div className='mdb-container-movies__search'>
-			<input className='mdb-container-movies__search-input'
+		<div className='mdb-movies__search right-icon'>
+			<button
+				type='sumbit'
+				className='mdb-form__search-button'
+				value='Ok'
+			>
+				<i className="fas fa-search"></i>
+			</button>
+			<input
+				className='mdb-movies__search-input'
 				type='text'
 				placeholder='Search...'
 			/>
-			<button className='mdb-container-form__search-button'>Ok</button>
 		</div>
 	);
 };
